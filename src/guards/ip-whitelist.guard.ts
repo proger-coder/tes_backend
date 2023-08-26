d1eaf2fa9a521cd236d6c54d5dbf8bab5067b7c5
Ayrat <DIMANVAZ04@GMAIL.COM>
2023-08-26 21:29:47 +0300

    продолжаем тесты.

--- src/guards/ip-whitelist.guard.ts
+++ src/guards/ip-whitelist.guard.ts
@@ -6,15 +6,20 @@ export class IpWhitelistGuard implements CanActivate {
   constructor(private readonly ipWhitelistService: IpWhitelistService) {}
 
   canActivate(context: ExecutionContext): boolean {
-    console.log('Guard is triggered for path:', context.switchToHttp().getRequest().path);
+    //console.log('Guard is triggered for path:', context.switchToHttp().getRequest().path);
+    console.log('context.getHandler() = ', context.getHandler());
+    console.log('context.getArgs() = ', context.getArgs());
+    console.log('context.getClass() = ', context.getClass());
     const request = context.switchToHttp().getRequest();
     // исключение для главной страницы
-    // if (request.path !== '/') {
-    //   throw new ForbiddenException('Доступ запрещён по списку IP');
-    // }
-    throw new ForbiddenException('гард блочит вообще всё!');
-    //return true;
-    //const clientIp = request.ip;
-    //return this.ipWhitelistService.check(clientIp);
+    if (request.path !== '/') {
+      return true;
+    }
+
+    const ipFromList = this.ipWhitelistService.check(request.ip);
+    if (ipFromList) {
+      return true;
+    }
+    throw new ForbiddenException('Доступ запрещён по списку IP');
   }
 }
